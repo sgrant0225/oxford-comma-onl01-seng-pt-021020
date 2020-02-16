fa703a4c2e4e5216e1fb346b58701f1ff6df85e1
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-16 03:42:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,4 +9,4 @@ def oxford_comma(array)
   end
  end
 
-
+#it's returning the array, then the -2 is counting the index from the end, so its placing a comma between the second and third word. then the addition is adding the "and" between the comma and last word which is also being counted from the end of the index -1
